94e06620e2b0e3d74fb59e20f0e766ebd4cac9c8
Artem <derenokartem95@gmail.com>
2021-10-24 21:20:34 +0300

    lessson 5 finish

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let a = prompt("Первое число?", 1);
-let b = prompt("Второе число?", 2);
+let nameJavaScript = prompt('Какое официальное название JavaScript');
 
-alert(+a + +b); 
+if (nameJavaScript === 'ECMAScript') {
+  alert('Верно');
+} else {
+  alert('Не верно. ECMAScript');
+}
+
+let number = +prompt('Введите число');
+
+if (number > 0) {
+  alert(1);
+} else if (number < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
+
+let result;
+let a = +prompt('Число А');
+let b = +prompt('Число B');
+
+(a + b < 4) ? result = 'Мало': result = 'Много';
+alert(result);
+
+
+let login = prompt('Введите логин');
+let message = (login === 'Сотрудник') ? 'Привет' :
+  (login === 'Директор') ? 'Здравствуйте' :
+  (login === '') ? 'Нет логина' :
+  '';
