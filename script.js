2a4793613b8c0f6b7e6ae205d987c45202b5e26b
Artem <derenokartem95@gmail.com>
2021-10-26 21:11:06 +0300

    lesson 6 done

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-alert('Я JavaScript');
+// let age = +prompt('Введите число');
+
+// if (age >= 14 && age <= 90) {
+//   alert('Верно');
+// } else {
+//   alert('Не верно');
+// }
+
+// if (age < 14 && age > 90) {
+//   alert('Верно');
+// } else {
+//   alert('Не верно');
+// }
+
+// if (!(age >= 14 && age <=90)) {
+//   alert('Верно');
+// } else {
+//   alert('Не верно');
+// }
+
+let userLogin = prompt('Кто там?');
+let userPassword;
+
+if (userLogin === 'Админ') {
+  if ((userPassword = prompt('Пароль?')) === 'Я главный') {
+    alert('Здравствуйте!');
+  } else if (userPassword !== 'Я главный' && userPassword !== null) {
+    alert('Неверный пароль');
+  } else {
+    alert('Отменено');
+  }
+} else if (userLogin !== 'Админ' && userLogin !== null) {
+  alert('Я вас не знаю');
+} else {
+  alert('Отменено');
+}
