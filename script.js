a3ffe89d0d7dd34624a1f5742e456e45bb6a1b87
Artem <derenokartem95@gmail.com>
2021-10-27 20:49:48 +0300

    lesson 10 done

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// function checkAge(age) {
-//   return (age > 18) ? true : confirm('Родители разрешили?');
-// }
+let ask = (question) => confirm(question) ? alert('Вы согласились') : alert('Вы отказались');
 
-function checkAge(age) {
-  return (age > 18) || confirm('Родители разрешали?');
-}
-
-function min(a, b) {
-  return Math.min(a, b);
-}
-
-function pow(x, n) {
-  if (isInteger(x) && isInteger(n)) {
-    return x ** n;
-  }
-}
-
-function isInteger(number) {
-  if (number % 1 === 0) {
-    return true;
-  }  
-  return false;
-}
+ask('Вы согласны на обработку данных?');
