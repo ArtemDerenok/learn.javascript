aed6e7b42381e50ad402b7381222b547c3c80af8
Artem <derenokartem95@gmail.com>
2021-10-27 20:28:25 +0300

    lesson 9 done

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-const browser = 'Edge';
+// function checkAge(age) {
+//   return (age > 18) ? true : confirm('Родители разрешили?');
+// }
 
-if (browser === 'Edge') {
-  alert('You have got the Edge!');
-} else if (browser === 'Chrome' 
-  || browser === 'Safari' 
-  || browser === 'Firefxo' 
-  || browser === 'Opera') {
-  alert('Okey we support these browsers too');
-} else {
-  alert('We hope that this page looks ok');
+function checkAge(age) {
+  return (age > 18) || confirm('Родители разрешали?');
 }
 
-const number = +prompt('Введите число между 0 и 3');
+function min(a, b) {
+  return Math.min(a, b);
+}
 
-switch (number) {
-  case 0:
-    alert('Вы ввели число 0');
-    break;
-  case 1:
-    alert('Вы ввели число 1');
-    break;
-  case 2:
-  case 3:
-    alert('Вы ввели число 2, а может и 3');
-    break;
-  default:
-    alert('Вы ввели неправильные данные');
+function pow(x, n) {
+  if (isInteger(x) && isInteger(n)) {
+    return x ** n;
+  }
+}
+
+function isInteger(number) {
+  if (number % 1 === 0) {
+    return true;
+  }  
+  return false;
 }
