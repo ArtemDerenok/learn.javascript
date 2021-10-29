4301b8a7c4a2fda68dcf198cc8fe2f6b5cecc61a
Artem <derenokartem95@gmail.com>
2021-10-29 22:19:44 +0300

    lesson 11 done

--- src/script.js
+++ src/script.js
@@ -1,4 +1,52 @@
-const ask = (question) =>
-  confirm(question) ? alert("Вы согласились") : alert("Вы отказались");
+const user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
 
-ask("Вы согласны на обработку данных?");
+const schedule = {};
+
+const isEmpty = (obj) => {
+  if (Object.keys(obj).length) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isEmpty(schedule));
+
+let sumSalary = 0;
+
+const calcSumSalary = (obj) => {
+  if (isEmpty(obj)) {
+    Object.values(obj).forEach((elem) => {
+      sumSalary += elem;
+    });
+    return sumSalary;
+  }
+  return false;
+};
+
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+console.log(calcSumSalary(salaries));
+
+const menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+const multiplyNumeric = (obj) => {
+  Object.keys(obj).forEach((elem) => {
+    if (typeof menu[elem] === "number") {
+      menu[elem] *= 2;
+    }
+  });
+};
+multiplyNumeric(menu);
+console.log(menu);
