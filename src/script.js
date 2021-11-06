273c65f50aedd50141d7525623aa2bae998767db
Artem <derenokartem95@gmail.com>
2021-11-06 19:28:23 +0300

    lesson 20 done

--- src/script.js
+++ src/script.js
@@ -1,29 +1,27 @@
-// const salaries = {
-//   John: 100,
-//   Pete: 300,
-//   Mary: 250,
+// Task 1
+
+// const user = {
+//   name: "John",
+//   years: 30,
 // };
 
-// const sumSalaries = (salary) => {
-//   let sum = 0;
-//   // eslint-disable-next-line no-restricted-syntax
-//   for (const value of Object.values(salary)) {
-//     sum += value;
-//   }
-//   return sum;
-// };
-
-// console.log(sumSalaries(salaries));
+// const { name, years: age, isAdmin = false } = user;
+// console.log(name);
+// console.log(age);
+// console.log(isAdmin);
 
 // Task 2
 
-const user = {
-  name: "John",
-  age: 30,
+const salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
 };
 
-const count = (obj) => {
-  return Object.values(obj).length;
+const topSalary = (obj) => {
+  const result = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+  const [name, ,] = result[0];
+  return name;
 };
 
-console.log(count(user));
+console.log(topSalary(salaries));
