291708cecbe303728d349c90be556f1a1e832e4d
Artem <derenokartem95@gmail.com>
2021-11-13 18:46:51 +0300

    lesson 27

--- src/script.js
+++ src/script.js
@@ -1,34 +1,108 @@
 // Task 1
 
-// const printNumbers = (from, to) => {
-//   let a = from;
+// function work(a, b) {
+//   console.log(a + b); // произвольная функция или метод
+// }
 
-//   const timerId = setInterval(() => {
-//     if (a <= to) {
-//       console.log(a);
-//       a += 1;
-//     } else {
-//       clearInterval(timerId);
-//     }
-//   }, 1000);
+// const spy = (func) => {
+//   function wrapper(...args) {
+//     wrapper.calls.push(args);
+//     return func.apply(this, arguments);
+//   }
+
+//   wrapper.calls = [];
+//   return wrapper;
 // };
 
-// printNumbers(1, 5);
+// work = spy(work);
+// work(1, 2); // 3
+// work(4, 5); // 9
+
+// for (const args of work.calls) {
+//   console.log(`call:${args.join()}`); // "call:1,2", "call:4,5"
+// }
+
 // Task 2
 
-let timerId = setTimeout(
-  function printNumbers(from, to) {
-    let a = from;
-    const b = to;
-    if (a <= b) {
-      console.log(from);
-      a += 1;
-      timerId = setTimeout(printNumbers, 1000, a, b);
-    } else {
-      clearTimeout(timerId);
+// function f(x) {
+//   console.log(x);
+// }
+
+// const delay = (func, ms) => {
+//   return (x) => {
+//     setTimeout(() => {
+//       func.call(this, x);
+//     }, ms);
+//   };
+// };
+
+// // создаём обёртки
+// const f1000 = delay(f, 1000);
+// const f1500 = delay(f, 1500);
+
+// f1000("test"); // показывает "test" после 1000 мс
+// f1500("test"); // показывает "test" после 1500 мс
+
+// Task 3
+
+// const debounce = (func, ms) => {
+//   let isCooldown = false;
+//   return function (x) {
+//     if (isCooldown) {
+//       return;
+//     }
+//     func.apply(this, arguments);
+//     isCooldown = true;
+//     setTimeout(() => (isCooldown = false), ms);
+//   };
+// };
+
+// const f = debounce(alert, 1000);
+
+// f(1); // выполняется немедленно
+// f(2); // проигнорирован
+
+// setTimeout(() => f(3), 100); // проигнорирован (прошло только 100 мс)
+// setTimeout(() => f(4), 1100); // выполняется
+// setTimeout(() => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
+
+// Task 4
+
+function f(a) {
+  console.log(a);
+}
+
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
     }
-  },
-  1000,
-  1,
-  5
-);
+
+    func.apply(this, arguments);
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false;
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
+
+// f1000 передаёт вызовы f максимум раз в 1000 мс
+const f1000 = throttle(f, 1000);
+
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
