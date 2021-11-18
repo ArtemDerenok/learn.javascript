fb43d657c8bc83d157e5c5eaaae3dc972c116e67
Artem <derenokartem95@gmail.com>
2021-11-18 23:00:38 +0300

    lesson 32 done

--- src/script.js
+++ src/script.js
@@ -32,5 +32,17 @@ class Clock {
   }
 }
 
-const clock = new Clock({ template: "h:m:s" });
+class ExtendetClock extends Clock {
+  constructor({ template }, precision = 1000) {
+    super({template});
+    this.precision = precision;
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+}
+
+const clock = new ExtendetClock({ template: "h:m:s" }, 10000);
 clock.start();
