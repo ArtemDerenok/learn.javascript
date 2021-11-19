fef655724cf30482a8634894a857157e924139d8
Artem <derenokartem95@gmail.com>
2021-11-19 22:40:39 +0300

    lesson 33 done

--- src/script.js
+++ src/script.js
@@ -1,48 +1,10 @@
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-
-  render() {
-    const date = new Date();
-    let hours = date.getHours();
-    if (hours < 10) hours = `0${hours}`;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = `0${mins}`;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = `0${secs}`;
-
-    const output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
+class Rabbit extends Object {
+  constructor(name) {
+    super();
+    this.name = name;
   }
 }
 
-class ExtendetClock extends Clock {
-  constructor({ template }, precision = 1000) {
-    super({template});
-    this.precision = precision;
-  }
+const rabbit = new Rabbit("Кроль");
 
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), this.precision);
-  }
-}
-
-const clock = new ExtendetClock({ template: "h:m:s" }, 10000);
-clock.start();
+alert(rabbit.hasOwnProperty("name")); // Ошибка
