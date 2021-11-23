8b7208859ded9429a231fdd066e026e219c8dec1
Artem <derenokartem95@gmail.com>
2021-11-23 22:47:11 +0300

    lesson 36 done

--- src/script.js
+++ src/script.js
@@ -1,27 +1,40 @@
-function showCircle(cx, cy, radius, callback) {
-  const div = document.createElement("div");
-  div.style.width = 0;
-  div.style.height = 0;
-  div.style.left = `${cx}px`;
-  div.style.top = `${cy}px`;
-  div.className = "circle";
-  document.body.append(div);
+// Task 2
 
-  setTimeout(() => {
-    div.style.width = `${radius * 2}px`;
-    div.style.height = `${radius * 2}px`;
+// function delay(ms) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, ms);
+//   });
+// }
 
-    div.addEventListener("transitionend", () => {
-      callback(div);
+// delay(3000).then(() => alert("выполнилось через 3 секунды"));
+
+// Task 3
+
+function showCircle(cx, cy, radius) {
+  return new Promise((resolve) => {
+    const div = document.createElement("div");
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = `${cx}px`;
+    div.style.top = `${cy}px`;
+    div.className = "circle";
+    document.body.append(div);
+
+    setTimeout(() => {
+      div.style.width = `${radius * 2}px`;
+      div.style.height = `${radius * 2}px`;
+    }, 0);
+    div.addEventListener("transitionend", function handler() {
+      div.removeEventListener("transitionend", handler);
+      resolve(div);
     });
-  }, 0);
+  });
 }
 
 const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-  showCircle(150, 150, 100, (div) => {
-    const circle = div;
-    circle.classList.add("message-ball");
-    circle.textContent = "Hello, world";
+  showCircle(150, 150, 100).then((div) => {
+    div.classList.add("message-ball");
+    div.append("Hello, world!");
   });
 });
