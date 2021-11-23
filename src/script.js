3363305e5937ceb8405eb40a9e70c5bf729e67ba
Artem <derenokartem95@gmail.com>
2021-11-23 22:17:28 +0300

    lesson 35 done

--- src/script.js
+++ src/script.js
@@ -1,15 +1,27 @@
-class FormatError extends SyntaxError {
-  constructor(message) {
-    super(message);
-    this.name = "FormatError";
-  }
+function showCircle(cx, cy, radius, callback) {
+  const div = document.createElement("div");
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = `${cx}px`;
+  div.style.top = `${cy}px`;
+  div.className = "circle";
+  document.body.append(div);
+
+  setTimeout(() => {
+    div.style.width = `${radius * 2}px`;
+    div.style.height = `${radius * 2}px`;
+
+    div.addEventListener("transitionend", () => {
+      callback(div);
+    });
+  }, 0);
 }
 
-const err = new FormatError("ошибка форматирования");
-
-alert(err.message); // ошибка форматирования
-alert(err.name); // FormatError
-alert(err.stack); // stack
-
-alert(err instanceof FormatError); // true
-alert(err instanceof SyntaxError); // true (потому что наследует от SyntaxError)
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  showCircle(150, 150, 100, (div) => {
+    const circle = div;
+    circle.classList.add("message-ball");
+    circle.textContent = "Hello, world";
+  });
+});
