d4be06bf2271be379f05f874024461ac95ec852c
Artem <derenokartem95@gmail.com>
2022-01-11 22:58:33 +0300

    lesson 38 done

--- src/script.js
+++ src/script.js
@@ -1,68 +1,74 @@
-// Task 1
+// Task1
 
-// async function loadJson(url) {
-//   let response = await fetch(url);
-//   if (response.status === 200) {
-//     let result = await response.json();
-//     return result;
-//   }
-//   throw new Error("Ошибка");
+// let user = {
+//   name: "John",
+// };
+
+// function wrap(target1) {
+//   return new Proxy(target1, {
+//     get(target2, prop, receiver) {
+//       if (prop in target2) {
+//         return Reflect.get(target2, prop, receiver);
+//       }
+//       throw new Error("Ошибка: такого свойства не существует");
+//     },
+//   });
 // }
 
-// loadJson("no-such-user.json")
-//   .catch(alert);
+// user = wrap(user);
+
+// alert(user.name);
+// alert(user.age);
 
 // Task 2
 
-// class HttpError extends Error {
-//   constructor(response) {
-//     super(`${response.status} for ${response.url}`);
-//     this.name = "HttpError";
-//     this.response = response;
-//   }
-// }
+// let array = [1, 2, 3];
 
-// async function loadJson(url) {
-//   let response = await fetch(url);
-//   if (response.status === 200) {
-//     let result = await response.json();
-//     return result;
-//   } else {
-//     throw new HttpError(response);
-//   }
-// }
+// array = new Proxy(array, {
+//   get(target, prop, receiver) {
+//     if (+prop < 0) {
+//       const value = target[target.length - Math.abs(prop)];
 
-// // Запрашивать логин, пока github не вернёт существующего пользователя.
-// async function demoGithubUser() {
-//   let response;
-//   while(true) {
-//     const name = prompt("Введите логин?", "iliakan");
-//     try{
-//       response = await loadJson(`https://api.github.com/users/${name}`);
-//       break;
-//     } catch(error) {
-//       if (error instanceof HttpError && error.response.status === 404) {
-//         alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
-//       } else {
-//         throw error;
+//       if (value) {
+//         return Reflect.get(target, target.length - Math.abs(prop), receiver);
 //       }
+//       throw new Error("Ошибка: такой элемент не существует");
+//     } else if (target[prop]) {
+//       return Reflect.get(target, prop, receiver);
+//     } else {
+//       throw new Error("Ошибка: такой элемент не существует");
 //     }
-//   }
-//   alert(`Полное имя: ${response.name}`);
-//   return response;
-// }
+//   },
+// });
 
-// demoGithubUser();
+// console.log(array[0]);
 
 // Task 3
+const handlers = Symbol("handlers");
 
-async function wait() {
-  await new Promise(resolve => setTimeout(resolve, 3000));
+function makeObservable(target) {
+  target[handlers] = [];
 
-  return 10;
+  target.observe = function (handler) {
+    target[handlers].push(handler);
+  };
+  return new Proxy(target, {
+    set(target, prop, value, receiver) {
+      const result = Reflect.set(target, prop, value, receiver);
+      if (result) {
+        target[handlers].forEach((handler) => handler(prop, value));
+      }
+      return result;
+    },
+  });
 }
 
-function f() {
-  wait().then(value => console.log(value));
-}
-f();
+let user = {};
+user = makeObservable(user);
+
+user.observe((key, value) => {
+  alert(`SET ${key}=${value}`);
+});
+
+user.name = "John";
+console.log(user);
