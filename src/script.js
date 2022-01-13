79baa5ff141231b8bcd4b2329519d923de9ed3e1
Artem <derenokartem95@gmail.com>
2022-01-13 22:50:44 +0300

    lesson 42 done

--- src/script.js
+++ src/script.js
@@ -1,15 +1,8 @@
 // Task 1
 
-// console.log(document.body.firstElementChild);
-// console.log(document.body.children[1]);
-// console.log(document.body.children[1].lastElementChild);
+const liCollection = document.querySelectorAll("li");
 
-// Task 3
-
-const table = document.body.children[2];
-
-const array = Array.from(table.rows);
-
-array.forEach((elem, index) => {
-  elem.cells[index].style.backgroundColor = "red";
-});
+for (const li of liCollection) {
+  console.log(li.firstChild.data);
+  console.log(li.getElementsByTagName('li').length);
+}
