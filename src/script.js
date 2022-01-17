c2614c3124c86e25f55da930d782aae54aeb4747
Artem <derenokartem95@gmail.com>
2022-01-17 22:04:00 +0300

    lesson 43 done

--- src/script.js
+++ src/script.js
@@ -1,8 +1,21 @@
 // Task 1
 
-const liCollection = document.querySelectorAll("li");
+// const divElement = document.querySelector("[data-widget-name]");
 
-for (const li of liCollection) {
-  console.log(li.firstChild.data);
-  console.log(li.getElementsByTagName('li').length);
-}
+// console.log(divElement.dataset.widgetName);
+
+// Task 2
+
+const links = document.querySelectorAll("a[href]");
+
+const linksArray = Array.from(links);
+
+linksArray.forEach((elem) => {
+  if (
+    elem.getAttribute("href").indexOf("://") > -1 &&
+    elem.getAttribute("href").indexOf("http://internal.com") === -1
+  ) {
+    elem.style.color = "orange";
+  }
+  console.log(elem.getAttribute("href").indexOf("://"));
+});
