3bb78b112c37a73be1a521eeaced8daa4ec20d17
Artem <derenokartem95@gmail.com>
2022-01-19 21:46:09 +0300

    lesson 46 done

--- src/script.js
+++ src/script.js
@@ -1,21 +1,13 @@
-const showNotification = ({ top = 0, right = 0, html, className}) => {
-  const div = document.createElement("div");
-  div.classList.add("notification");
-  if (className) {
-    div.classList.add(className);
-  }
-  div.style.top = `${top}px`;
-  div.style.right = `${right}px`;
-  div.textContent = html;
-  document.body.append(div);
-};
-showNotification({
-  top: 10, // 10px от верхней границы окна (по умолчанию 0px)
-  right: 10, // 10px от правого края окна (по умолчанию 0px)
-  html: "Hello!", // HTML-уведомление
-  className: "welcome",
-});
+// task 2
 
-setTimeout(() => {
-  document.querySelector("div").style.display = "none";
-}, 1500);
+// let scrollWidth = div.offsetWidth - div.clientWidth;
+
+// Task 3
+
+const field = document.getElementById("field");
+const ball = document.getElementById("ball");
+
+console.log(field.offsetWidth);
+
+ball.style.left = `${field.clientWidth / 2 - ball.clientWidth / 2}px`;
+ball.style.top = `${field.clientHeight / 2 - ball.clientHeight / 2}px`;
