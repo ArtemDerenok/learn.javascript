bd794026c7ee161665df827b5e09007b70956ec4
Artem <derenokartem95@gmail.com>
2022-01-25 17:18:17 +0300

    lesson 49 done

--- src/script.js
+++ src/script.js
@@ -1,98 +1,108 @@
 // Task 1
 
-// const btn = document.getElementById("hider");
+// const container = document.getElementById("container");
 
-// btn.addEventListener("click", () => {
-//   document.getElementById("text").hidden = true;
+// container.addEventListener("click", (event) => {
+//   if (event.target.tagName !== "BUTTON") {
+//     return;
+//   }
+//   const pane = event.target.closest("div");
+//   pane.remove();
 // });
 
 // Task 2
 
-// const btn = document.getElementById("hider");
+// const tree = document.getElementById("tree");
 
-// btn.addEventListener("click", function hider() {
-//   this.hidden = true;
+// console.dir(tree);
+
+// tree.addEventListener("click", (event) => {
+//   if (event.target.tagName !== "LI") {
+//     return;
+//   }
+//   if (event.target.firstElementChild) {
+//     event.target.firstElementChild.hidden =
+//       !event.target.firstElementChild.hidden;
+//   }
+// });
+
+// tree.addEventListener("mousemove", (event) => {
+//   if (event.target.tagName !== "LI") {
+//     return;
+//   }
+//   event.target.classList.add("bold");
+// });
+
+// tree.addEventListener("mouseout", (event) => {
+//   if (event.target.tagName !== "LI") {
+//     return;
+//   }
+//   event.target.classList.remove("bold");
+// });
+
+// Task 3
+
+// const table = document.getElementById("grid");
+// const tbody = document.querySelector("tbody");
+
+// const sortNames = (names) => {
+//   const array = Array.from(names);
+//   const result = array.sort((a, b) => {
+//     if (a.lastElementChild.textContent > b.lastElementChild.textContent) {
+//       return 1;
+//     }
+//     if (a.lastElementChild.textContent < b.lastElementChild.textContent) {
+//       return -1;
+//     }
+//     return 0;
+//   });
+//   return result;
+// };
+
+// const sortAges = (ages) => {
+//   const array = Array.from(ages);
+//   const result = array.sort((a, b) => {
+//     return +a.firstElementChild.textContent - +b.firstElementChild.textContent;
+//   });
+//   return result;
+// };
+
+// table.addEventListener("click", (event) => {
+//   if (event.target.tagName !== "TH") {
+//     return;
+//   }
+
+//   if (event.target.dataset.type === "string") {
+//     tbody.append(...sortNames(document.querySelectorAll("tbody tr")));
+//   }
+
+//   if (event.target.dataset.type === "number") {
+//     tbody.append(...sortAges(document.querySelectorAll("tbody tr")));
+//   }
 // });
 
 // Task 4
 
-// const field = document.getElementById("field");
-// const ball = document.getElementById("ball");
+document.addEventListener("mouseover", (event) => {
+  if (event.target.tagName !== "BUTTON") {
+    return;
+  }
 
-// const checkFieldSize = (num) => {
-//   if (num >= field.clientWidth - ball.clientWidth) {
-//     return field.clientWidth - ball.clientWidth;
-//   }
-//   if (num >= field.clientHeight - ball.clientHeight) {
-//     return field.clientHeight - ball.clientHeight;
-//   }
-//   if (num <= 0) {
-//     return 0;
-//   }
-//   return num;
-// };
+  const span = document.createElement("span");
+  span.classList.add("tooltip");
+  span.innerHTML = event.target.dataset.tooltip;
+  const coord = event.target.getBoundingClientRect();
 
-// const caclCoord = (num1, num2) => {
-//   const result = num1 - num2 / 2;
-//   return checkFieldSize(result);
-// };
-
-// field.addEventListener("click", (event) => {
-//   console.log(event);
-//   ball.style.left = `${caclCoord(event.offsetX, ball.offsetWidth)}px`;
-//   ball.style.top = `${caclCoord(event.offsetY, ball.offsetHeight)}px`;
-// });
-
-// Task 5
-
-// const showMenuBtn = document.querySelector(".title");
-
-// showMenuBtn.addEventListener("click", () => {
-//   showMenuBtn.nextElementSibling.classList.toggle("hidden");
-//   document.querySelector(".menu").classList.toggle("open");
-// });
-
-// Task 6
-
-// const planes = document.querySelectorAll(".pane");
-
-// planes.forEach((elem) => {
-//   elem.firstElementChild.insertAdjacentHTML(
-//     "beforeend",
-//     '<button class="remove-button">[x]</button>'
-//   );
-// });
-
-// const btns = document.querySelectorAll(".remove-button");
-
-// btns.forEach((elem) => {
-//   elem.addEventListener("click", () => {
-//     elem.parentElement.parentElement.remove();
-//   });
-// });
-
-// Task 7
-
-const btnNext = document.querySelector(".next");
-const btnPrev = document.querySelector(".prev");
-const list = document.querySelector("ul");
-const listElems = document.querySelectorAll("li");
-
-const width = 130;
-const count = 3;
-
-let position = 0;
-
-btnNext.addEventListener("click", () => {
-  position -= width * count;
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.left = `${position}px`;
-  console.log(position);
+  span.style.left = `${coord.x}px`;
+  span.style.top = `${coord.bottom + 5}px`;
+  document.body.append(span);
 });
 
-btnPrev.addEventListener("click", () => {
-  position += width * count;
-  position = Math.min(position, 0);
-  list.style.left = `${position}px`;
-  console.log(position);
+document.addEventListener("mouseout", (event) => {
+  if (event.target.tagName !== "BUTTON") {
+    return;
+  }
+
+  const span = document.querySelector("span");
+  span.remove();
 });
