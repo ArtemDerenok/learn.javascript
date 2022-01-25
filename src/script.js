5237d6fa8468d61e22ec08ff1f4d3b3fe7d3c6fe
Artem <derenokartem95@gmail.com>
2022-01-25 18:07:36 +0300

    lesson 50 done

--- src/script.js
+++ src/script.js
@@ -1,108 +1,27 @@
-// Task 1
-
-// const container = document.getElementById("container");
-
-// container.addEventListener("click", (event) => {
-//   if (event.target.tagName !== "BUTTON") {
-//     return;
-//   }
-//   const pane = event.target.closest("div");
-//   pane.remove();
-// });
-
 // Task 2
 
-// const tree = document.getElementById("tree");
+// const content = document.getElementById("contents");
 
-// console.dir(tree);
-
-// tree.addEventListener("click", (event) => {
-//   if (event.target.tagName !== "LI") {
+// content.addEventListener("click", (event) => {
+//   if (!event.target.closest("A")) {
 //     return;
 //   }
-//   if (event.target.firstElementChild) {
-//     event.target.firstElementChild.hidden =
-//       !event.target.firstElementChild.hidden;
-//   }
-// });
 
-// tree.addEventListener("mousemove", (event) => {
-//   if (event.target.tagName !== "LI") {
-//     return;
+//   const question = confirm("Вы действительно хотите перейти по ссылке?");
+//   if (!question) {
+//     event.preventDefault();
 //   }
-//   event.target.classList.add("bold");
-// });
-
-// tree.addEventListener("mouseout", (event) => {
-//   if (event.target.tagName !== "LI") {
-//     return;
-//   }
-//   event.target.classList.remove("bold");
 // });
 
 // Task 3
 
-// const table = document.getElementById("grid");
-// const tbody = document.querySelector("tbody");
+const largeImage = document.getElementById("largeImg");
+const thumb = document.getElementById("thumbs");
 
-// const sortNames = (names) => {
-//   const array = Array.from(names);
-//   const result = array.sort((a, b) => {
-//     if (a.lastElementChild.textContent > b.lastElementChild.textContent) {
-//       return 1;
-//     }
-//     if (a.lastElementChild.textContent < b.lastElementChild.textContent) {
-//       return -1;
-//     }
-//     return 0;
-//   });
-//   return result;
-// };
-
-// const sortAges = (ages) => {
-//   const array = Array.from(ages);
-//   const result = array.sort((a, b) => {
-//     return +a.firstElementChild.textContent - +b.firstElementChild.textContent;
-//   });
-//   return result;
-// };
-
-// table.addEventListener("click", (event) => {
-//   if (event.target.tagName !== "TH") {
-//     return;
-//   }
-
-//   if (event.target.dataset.type === "string") {
-//     tbody.append(...sortNames(document.querySelectorAll("tbody tr")));
-//   }
-
-//   if (event.target.dataset.type === "number") {
-//     tbody.append(...sortAges(document.querySelectorAll("tbody tr")));
-//   }
-// });
-
-// Task 4
-
-document.addEventListener("mouseover", (event) => {
-  if (event.target.tagName !== "BUTTON") {
+thumb.addEventListener("click", (event) => {
+  if (!event.target.closest("A")) {
     return;
   }
-
-  const span = document.createElement("span");
-  span.classList.add("tooltip");
-  span.innerHTML = event.target.dataset.tooltip;
-  const coord = event.target.getBoundingClientRect();
-
-  span.style.left = `${coord.x}px`;
-  span.style.top = `${coord.bottom + 5}px`;
-  document.body.append(span);
-});
-
-document.addEventListener("mouseout", (event) => {
-  if (event.target.tagName !== "BUTTON") {
-    return;
-  }
-
-  const span = document.querySelector("span");
-  span.remove();
+  largeImage.src = event.target.parentElement.href;
+  event.preventDefault();
 });
