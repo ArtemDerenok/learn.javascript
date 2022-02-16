f9b684eb17ee801023574ded3810d3595c5fa1ba
Artem <derenokartem95@gmail.com>
2022-02-16 17:16:42 +0300

    lesson 54 done

--- src/script.js
+++ src/script.js
@@ -1,83 +1,20 @@
-// Task 1
+const runOnKeys = (fn, code1, code2) => {
+  const set = new Set();
 
-// const thumb = document.querySelector(".thumb");
-// const slider = document.getElementById("slider");
+  document.body.addEventListener("keydown", (event) => {
+    set.add(event.code);
+  });
 
-// thumb.addEventListener("mousedown", (event) => {
-//   event.preventDefault();
-//   const shiftX = event.clientX - thumb.getBoundingClientRect().left;
-
-//   function onMouseMove(event) {
-//     let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
-//     if (newLeft < 0) newLeft = 0;
-//     const rightEdge = slider.offsetWidth - thumb.offsetWidth;
-//     if (newLeft > rightEdge) newLeft = rightEdge;
-//     thumb.style.left = `${newLeft}px`;
-//   }
-
-//   document.addEventListener("mousemove", onMouseMove);
-
-//   function offMouseMove() {
-//     document.removeEventListener("mousemove", onMouseMove);
-//     document.removeEventListener("mouseup", offMouseMove);
-//   }
-
-//   document.addEventListener("mouseup", offMouseMove);
-// });
-
-// thumb.ondragstart = function () {
-//   return false;
-// };
-
-// Task 2
-
-const ball = document.getElementById("ball");
-
-document.body.addEventListener("mousedown", (event) => {
-  if (event.target.closest(".draggable")) {
-    const hero = event.target.closest(".draggable");
-
-    if (event.target.closest("#ball")) {
-      hero.style.zIndex = 1000;
+  document.body.addEventListener("keyup", (event) => {
+    if (set.has(code1) && set.has(code2)) {
+      fn();
     }
-    const shiftX = event.clientX - hero.getBoundingClientRect().left;
-    const shiftY = event.clientY - hero.getBoundingClientRect().top;
+    set.clear();
+  });
+};
 
-    function onMouseMove(event) {
-      let leftNewPosition = event.clientX - shiftX;
-      let topNewPosition = event.clientY - shiftY;
-      const rightEdge = document.documentElement.clientWidth - hero.offsetWidth;
-      const bottomEdge =
-        document.documentElement.clientHeight - hero.offsetHeight;
+function func() {
+  console.log("Привет");
+}
 
-      if (leftNewPosition < 0) {
-        leftNewPosition = 0;
-      } else if (leftNewPosition > rightEdge) {
-        leftNewPosition = rightEdge;
-      }
-
-      if (topNewPosition < 0) {
-        topNewPosition = 0;
-      } else if (topNewPosition > bottomEdge) {
-        topNewPosition = bottomEdge;
-      }
-
-      hero.style.left = `${leftNewPosition}px`;
-      hero.style.top = `${topNewPosition}px`;
-      hero.style.position = "absolute";
-    }
-    document.addEventListener("mousemove", onMouseMove);
-
-    function offMouseMove() {
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", offMouseMove);
-    }
-
-    document.addEventListener("mouseup", offMouseMove);
-  }
-});
-
-ball.addEventListener("dragstart", (event) => {
-  event.preventDefault();
-});
+runOnKeys(func, "KeyZ", "KeyX");
