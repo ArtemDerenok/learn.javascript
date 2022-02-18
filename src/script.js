3800a63df07aba64b020ed3d5e1caa2788606c39
Artem <derenokartem95@gmail.com>
2022-02-18 20:20:24 +0300

    lesson 55 done

--- src/script.js
+++ src/script.js
@@ -1,20 +1,69 @@
-const runOnKeys = (fn, code1, code2) => {
-  const set = new Set();
+// Task 1
 
-  document.body.addEventListener("keydown", (event) => {
-    set.add(event.code);
-  });
-
-  document.body.addEventListener("keyup", (event) => {
-    if (set.has(code1) && set.has(code2)) {
-      fn();
+function populate() {
+  while (true) {
+    const windowMarginBottom =
+      document.documentElement.getBoundingClientRect().bottom;
+    if (windowMarginBottom < document.documentElement.clientHeight + 100) {
+      document.body.insertAdjacentHTML(
+        "beforeend",
+        `<p>Дата: ${new Date()}</p>`
+      );
+    } else {
+      break;
     }
-    set.clear();
-  });
-};
-
-function func() {
-  console.log("Привет");
+  }
 }
 
-runOnKeys(func, "KeyZ", "KeyX");
+window.addEventListener("scroll", populate);
+
+populate();
+
+// Task 2
+
+// window.addEventListener("scroll", () => {
+//   const winodwHeigh = document.documentElement.clientHeight;
+//   if (pageYOffset > winodwHeigh) {
+//     document.getElementById("arrowTop").style.visibility = "visible";
+//     document.getElementById("arrowTop").addEventListener(
+//       "click",
+//       () => {
+//         document.documentElement.scrollTop = 0;
+//       },
+//       { once: true }
+//     );
+//   } else {
+//     document.getElementById("arrowTop").style.visibility = "hidden";
+//   }
+// });
+
+// Task 3
+// function isVisible(elem) {
+//   if (
+//     elem.getBoundingClientRect().top < document.documentElement.clientHeight
+//   ) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// function showVisible() {
+//   for (const img of document.querySelectorAll("img")) {
+//     let realSrc = img.dataset.src;
+
+//     if (!realSrc) {
+//       continue;
+//     }
+
+//     if (isVisible(img)) {
+//       realSrc += `?nocache=${Math.random()}`;
+
+//       img.src = realSrc;
+
+//       img.dataset.src = "";
+//     }
+//   }
+// }
+
+// window.addEventListener("scroll", showVisible);
+// showVisible();
