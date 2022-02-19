887fc0c7c991e790ccfa517d2be491e017b42e8e
Artem <derenokartem95@gmail.com>
2022-02-19 17:29:46 +0300

    lesson 57 done

--- src/script.js
+++ src/script.js
@@ -1,69 +1,7 @@
-// Task 1
+const selectGenres = document.getElementById("genres");
 
-function populate() {
-  while (true) {
-    const windowMarginBottom =
-      document.documentElement.getBoundingClientRect().bottom;
-    if (windowMarginBottom < document.documentElement.clientHeight + 100) {
-      document.body.insertAdjacentHTML(
-        "beforeend",
-        `<p>Дата: ${new Date()}</p>`
-      );
-    } else {
-      break;
-    }
-  }
-}
+console.log(selectGenres.value);
+console.log(selectGenres[selectGenres.selectedIndex].text);
 
-window.addEventListener("scroll", populate);
-
-populate();
-
-// Task 2
-
-// window.addEventListener("scroll", () => {
-//   const winodwHeigh = document.documentElement.clientHeight;
-//   if (pageYOffset > winodwHeigh) {
-//     document.getElementById("arrowTop").style.visibility = "visible";
-//     document.getElementById("arrowTop").addEventListener(
-//       "click",
-//       () => {
-//         document.documentElement.scrollTop = 0;
-//       },
-//       { once: true }
-//     );
-//   } else {
-//     document.getElementById("arrowTop").style.visibility = "hidden";
-//   }
-// });
-
-// Task 3
-// function isVisible(elem) {
-//   if (
-//     elem.getBoundingClientRect().top < document.documentElement.clientHeight
-//   ) {
-//     return true;
-//   }
-//   return false;
-// }
-
-// function showVisible() {
-//   for (const img of document.querySelectorAll("img")) {
-//     let realSrc = img.dataset.src;
-
-//     if (!realSrc) {
-//       continue;
-//     }
-
-//     if (isVisible(img)) {
-//       realSrc += `?nocache=${Math.random()}`;
-
-//       img.src = realSrc;
-
-//       img.dataset.src = "";
-//     }
-//   }
-// }
-
-// window.addEventListener("scroll", showVisible);
-// showVisible();
+const option = new Option("Классика", "classic", false, true);
+selectGenres.append(option);
