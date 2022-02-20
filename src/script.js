5ab882995d864e0639b9ee442b39d65ff84a1e80
Artem <derenokartem95@gmail.com>
2022-02-20 18:15:39 +0300

    lesson 58 done

--- src/script.js
+++ src/script.js
@@ -1,98 +1,28 @@
-// Task 1
+const form = document.getElementById("form");
+const beforMoney = document.getElementById("money-before");
+const afterMoney = document.getElementById("money-after");
+const afterGraph = document.getElementById("height-after");
 
-// const viewDiv = document.getElementById("view");
-// const textArea = document.createElement("textarea");
-
-// viewDiv.addEventListener("click", function () {
-//   this.replaceWith(textArea);
-//   textArea.classList.add("edit");
-//   textArea.value = this.textContent;
-//   textArea.focus();
-// });
-
-// textArea.addEventListener("blur", function () {
-//   viewDiv.textContent = this.value;
-//   this.replaceWith(viewDiv);
-// });
-
-// Task 2
-
-// const table = document.getElementById("bagua-table");
-// const textArea = document.createElement("textarea");
-// const buttonOkey = document.createElement("button");
-// const cancelButton = document.createElement("button");
-// buttonOkey.textContent = "OK";
-// cancelButton.textContent = "CANCEL";
-
-// function addTextArea(event) {
-//   const td = event.target.closest("td");
-//   let oldInnerHtml = "";
-//   if (td) {
-//     textArea.value = td.innerHTML;
-//     oldInnerHtml = td.innerHTML;
-//     td.replaceChildren(textArea);
-//     td.append(buttonOkey);
-//     td.append(cancelButton);
-//     buttonOkey.addEventListener(
-//       "click",
-//       () => {
-//         td.innerHTML = textArea.value;
-//         table.addEventListener("click", addTextArea);
-//       },
-//       { once: true }
-//     );
-//     cancelButton.addEventListener(
-//       "click",
-//       () => {
-//         td.innerHTML = oldInnerHtml;
-//         table.addEventListener("click", addTextArea);
-//       },
-//       { once: true }
-//     );
-//     table.removeEventListener("click", addTextArea);
-//   }
-// }
-
-// table.addEventListener("click", addTextArea);
-
-// Task 3
-
-const mouse = document.getElementById("mouse");
-
-const move = (event) => {
-  mouse.style.position = "absolute";
-  console.log(event);
-  if (event.key === "ArrowRight") {
-    mouse.style.left = `${mouse.getBoundingClientRect().left + 100}px`;
-  } else if (event.key === "ArrowLeft") {
-    mouse.style.left = `${mouse.getBoundingClientRect().left - 100}px`;
-  } else if (event.key === "ArrowUp") {
-    mouse.style.top = `${mouse.getBoundingClientRect().top - 100}px`;
-  } else if (event.key === "ArrowDown") {
-    mouse.style.top = `${mouse.getBoundingClientRect().top + 100}px`;
-  }
-  mouse.addEventListener(
-    "blur",
-    () => {
-      mouse.removeEventListener("keyup", move);
-      mouse.addEventListener(
-        "click",
-        () => {
-          mouse.focus();
-          mouse.addEventListener("keyup", move);
-        },
-        { once: true }
-      );
-    },
-    { once: true }
-  );
+const changeGraph = (difference, initial) => {
+  const graphValue = difference * 100 / initial;
+  afterGraph.style.height = `${100 + graphValue}px`;
 };
 
-mouse.addEventListener(
-  "click",
-  () => {
-    mouse.focus();
-    mouse.addEventListener("keyup", move);
-  },
-  { once: true }
-);
+const showResult = (befor, after) => {
+  beforMoney.textContent = befor;
+  afterMoney.textContent = after;
+};
+
+const calculateDeposite = (initial, interest, years) => {
+  interest = interest * 0.01;
+  years = years / 12;
+  const result = Math.round(initial * (1 + interest) ** years);
+  showResult(initial, result);
+  changeGraph(result - initial, initial);
+};
+
+calculateDeposite(+form.elements[0].value, +form.elements[2].value , +form.elements[1].value);
+
+form.addEventListener("input", () => {
+  calculateDeposite(+form.elements[0].value, +form.elements[2].value , +form.elements[1].value);
+});
