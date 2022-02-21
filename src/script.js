c698a43ba5aa264fd16a9e5f36c860c99f2cf088
Artem <derenokartem95@gmail.com>
2022-02-21 22:13:43 +0300

    lesson 60 done

--- src/script.js
+++ src/script.js
@@ -1,26 +1,43 @@
-const showFormBtn = document.getElementById("show-form-btn");
-const formContainer = document.getElementById("prompt-form-container");
-const form = document.getElementById("prompt-form");
-const formMessage = document.getElementById("prompt-message");
+function preloadImages(sources, callback) {
+  let cnt = 0;
+  sources.forEach((elem) => {
+    const img = document.createElement("img");
+    img.src = elem;
+    img.addEventListener("load", () => {
+      cnt += 1;
+      if (cnt === sources.length) {
+        callback();
+      }
+    });
 
-const showForm = (message, callback) => {
-  formMessage.innerHTML = message;
-  formContainer.style.display = "block";
-  form.addEventListener("submit", callback, { once: true });
-  form.elements[2].addEventListener(
-    "click",
-    () => {
-      console.log(null);
-      formContainer.style.display = "none";
-    },
-    { once: true }
-  );
-};
-
-showFormBtn.addEventListener("click", () => {
-  showForm("Введите что-нибудь<br>...умное :)", function (event) {
-    event.preventDefault();
-    console.log(form.elements[0].value);
-    formContainer.style.display = "none";
+    img.addEventListener("error", () => alert("Ошибка загрузки изображения"));
   });
-});
+}
+
+// ---------- тест ----------
+
+const sources = [
+  "https://en.js.cx/images-load/1.jpg",
+  "https://en.js.cx/images-load/2.jpg",
+  "https://en.js.cx/images-load/3.jpg",
+];
+
+// добавляем случайные символы к ссылкам, чтобы избежать кеширования
+for (let i = 0; i < sources.length; i += 1) {
+  sources[i] += `?${Math.random()}`;
+}
+
+// для каждого изображения
+// создадим другое изображение с аналогичным src и проверим, есть ли у нас его ширина
+function testLoaded() {
+  let widthSum = 0;
+  for (let i = 0; i < sources.length; i += 1) {
+    const img = document.createElement("img");
+    img.src = sources[i];
+    widthSum += img.width;
+  }
+  alert(widthSum);
+}
+
+// каждое изображение в разрешении 100x100, итоговая сумма их ширин должна быть 300
+preloadImages(sources, testLoaded);
