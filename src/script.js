b9f99c289d00027f169021de0cfecde9574a7c2b
Artem <derenokartem95@gmail.com>
2022-02-28 22:32:46 +0300

    lesson 61 done

--- src/script.js
+++ src/script.js
@@ -1,43 +1,14 @@
-function preloadImages(sources, callback) {
-  let cnt = 0;
-  sources.forEach((elem) => {
-    const img = document.createElement("img");
-    img.src = elem;
-    img.addEventListener("load", () => {
-      cnt += 1;
-      if (cnt === sources.length) {
-        callback();
-      }
-    });
-
-    img.addEventListener("error", () => alert("Ошибка загрузки изображения"));
+const getUsers = async (userNames) => {
+  const result = [];
+  userNames.forEach(async (elem) => {
+    const response = await fetch(`https://api.github.com/users/${elem}`);
+    if (response.status === 200) {
+      result.push(await response.json());
+    } else {
+      throw new Error('user is not found');
+    }
   });
-}
+  return result;
+};
 
-// ---------- тест ----------
-
-const sources = [
-  "https://en.js.cx/images-load/1.jpg",
-  "https://en.js.cx/images-load/2.jpg",
-  "https://en.js.cx/images-load/3.jpg",
-];
-
-// добавляем случайные символы к ссылкам, чтобы избежать кеширования
-for (let i = 0; i < sources.length; i += 1) {
-  sources[i] += `?${Math.random()}`;
-}
-
-// для каждого изображения
-// создадим другое изображение с аналогичным src и проверим, есть ли у нас его ширина
-function testLoaded() {
-  let widthSum = 0;
-  for (let i = 0; i < sources.length; i += 1) {
-    const img = document.createElement("img");
-    img.src = sources[i];
-    widthSum += img.width;
-  }
-  alert(widthSum);
-}
-
-// каждое изображение в разрешении 100x100, итоговая сумма их ширин должна быть 300
-preloadImages(sources, testLoaded);
+getUsers(['artemderenok', 'mir97']).then(result => console.log(result));
